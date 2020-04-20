aed34ca0d9c822fd1c327009d479f6cf8e1b7f4d
Agathamoraes <agatha_cristie16@hotmail.com>
2020-04-19 22:17:26 -0300

    nome commit

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Post
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth import  authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+
+@login_required(login_url='/login/')
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
     return render(request, 'blog/post_list.html', {})
+
+def logout_user (request):
+    logout (request)
+    return redirect ('/login/')
+
+def login_user (request):
+    return render (request, 'login.html')
+@csrf_protect
+def submit_login (request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate (username = username, password = password)
+        if user is not None:
+            login (request, user)
+            return redirect ('/')
+        else:
+            messages.error (request, 'Usuário ou senha Inválido. Tente novamente')
+    return redirect ('/login/')
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from blog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user),
+    path('/', views.post_list)
+
 ]
